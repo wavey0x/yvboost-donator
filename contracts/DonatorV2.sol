36c68c1d5eb6428c57d643d198d58724b1bc705a
wavey0x <retrowaveyy@gmail.com>
2022-10-20 12:52:15 -0400

    feat: sec review

--- contracts/DonatorV2.sol
+++ contracts/DonatorV2.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -14,7 +12,6 @@ interface IStrategy {
 
 contract Donator {
     using SafeERC20 for IERC20;
-    using Address for address;
 
     event Donated(address strategy, uint256 amount, uint256 period);
 
